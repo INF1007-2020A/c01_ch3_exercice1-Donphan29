1f98bc64f99e5fb327afaae7d2ef2bf26f3d17e9
Donphan29 <huy-don.phan@polymtl.ca>
2020-09-09 19:41:53 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,7 @@ def square(number: int) -> int:
 
 def main() -> None:
     for i in range(25):
-        print(f"Square root: {square_root(i)}, square: {square(i)}")
+        print(f"Square root: {square_root(i)}, Square: {square(i)}")
 
 
 if __name__ == '__main__':
